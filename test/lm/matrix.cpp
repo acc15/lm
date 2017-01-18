0d20eb05a083cc912a85829f6b543529f176fc3a
rshift <utvk@ya.ru>
2017-01-18 03:10:14 +0300

    lu_decomp

--- test/lm/matrix.cpp
+++ test/lm/matrix.cpp
@@ -5,13 +5,40 @@ using namespace lm;
 
 TEST_CASE("lu_decomposition", "[matrix]") {
 
-    float m[3][3] = {
-        {0, 1, 2},
-        {3, 4, 5},
-        {6, 7, 8}
+
+    float test_matricies[][3][3] = {
+        {
+            {  9,  1,  2 },
+            {  3,  4,  5 },
+            {  6,  7,  8 }
+        },
+        {
+            {  1, -2,  3 },
+            {  4,  0,  6 },
+            { -7,  8,  9 }
+        },
+        {
+            {  3,  3, -1 },
+            {  4,  1,  3 },
+            {  1, -2, -2 }
+        },
     };
 
-    float p[3];
+    float expected_det[] = { -27.f, 204.f, 54.f };
+    for (size_t i = 0; i < sizeof(expected_det) / sizeof(float); i++) {
 
-    lu_decomposition(m, p);
+        permutation_matrix<float (&)[3][3], size_t[3]> m(test_matricies[i]);
+
+        REQUIRE( lu_decomposition(m) );
+
+        float det = m.cell(0, 0) * m.cell(1, 1) * m.cell(2, 2);
+        if (m.permutation_count() & 1 != 0) {
+            det = -det;
+        }
+
+        REQUIRE( det == expected_det[i] );
+
+    }
+
+
 }
