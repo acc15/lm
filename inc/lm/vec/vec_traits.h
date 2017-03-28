417d70120dedc5d861177bcab755e2cda00d3525
rshift <utvk@ya.ru>
2017-03-29 01:57:53 +0300

    vec_traits

--- inc/lm/vec/vec_traits.h
+++ inc/lm/vec/vec_traits.h
@@ -1,18 +1,42 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
+#include <utility>
 
 #include <lm/util/assert.h>
 
 namespace lm {
+
+template <typename T, typename = void>
+struct is_resizable {
+    typedef T type;
+    enum {
+        value = false
+    };
+};
+
+template <typename T>
+struct is_resizable<T, decltype( std::declval<T>().resize(10) ) > {
+    typedef T type;
+    enum {
+        value = true
+    };
+};
 
 template <typename T>
 struct vec_traits {
 
     typedef T type;
 
-    static void resize(type& vec, size_t new_size) {
+    template <typename U = T>
+    static typename std::enable_if<is_resizable<U>::value>::type resize(U& vec, size_t new_size) {
         vec.resize(new_size);
+    }
+
+    template <typename U = T>
+    static typename std::enable_if<!is_resizable<U>::value>::type resize(U& vec, size_t new_size) {
+        lm_assert( vec.size() == new_size, "static containers can't be resized" );
     }
 
     static size_t size(const type& vec) {
--- test/lm/vec_traits.cpp
+++ test/lm/vec_traits.cpp
@@ -2,15 +2,29 @@
 #include <lm/vec/vec_traits.h>
 
 #include <array>
-#include <exception>
+#include <vector>
 
-TEST_CASE("vec_traits", "[array]") {
-
+TEST_CASE("array", "[vec_traits]") {
     typedef lm::vec_traits<int[32]> traits;
 
     int v[32];
-
     REQUIRE( traits::size(v) == 32 );
-
     REQUIRE_THROWS( traits::resize(v, 10) );
+}
+
+TEST_CASE("std::array", "[vec_traits]") {
+    typedef lm::vec_traits<std::array<int, 32>> traits;
+
+    std::array<int, 32> v;
+    REQUIRE( traits::size(v) == 32 );
+    REQUIRE_THROWS( traits::resize(v, 10) );
+}
+
+TEST_CASE("std::vector", "[vec_traits]") {
+    typedef lm::vec_traits<std::vector<int>> traits;
+
+    std::vector<int> v = {1,2,3};
+    REQUIRE( traits::size(v) == 3 );
+    traits::resize(v, 10);
+    REQUIRE( traits::size(v) == 10 );
 }
